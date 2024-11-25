69734fec815150a5e93ea61fbbb909dab5197d3e
mteXD <matic.pozenel@gmail.com>
2024-11-25 03:40:32 +0100

    Basic server setup

---
+++ image-files/cmd/api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"image-service/internal/env"
+	"log"
+    "github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	cfg := config{
+		addr: env.GetString("ADDR", ":8080"),
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	mux := app.mount()
+
+	log.Fatal(app.run(mux))
+}
