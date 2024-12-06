e7dc3f2b4a8a5d40d9b65a616bbeeb080ef212e4
ba-na-na-na-na <vojko.hysz@gmail.com>
2024-12-06 20:58:13 +0100

    delete implemented

--- organisations/src/main/java/si/justphotons/organisations/api/v1/OrganisationsResource.java
+++ organisations/src/main/java/si/justphotons/organisations/api/v1/OrganisationsResource.java
@@ -3,6 +3,7 @@ package si.justphotons.organisations.api.v1;
 import si.justphotons.organisations.entities.Organisation;
 import si.justphotons.organisations.services.beans.OrganisationsBean;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,8 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -25,23 +30,42 @@ public class OrganisationsResource {
 		this.organisationsBean = organisationsBean;
 	}
 
-	@GetMapping("/")
+	@GetMapping
 	public ResponseEntity<List<Organisation>> getAll() {
 		List<Organisation> orgs = organisationsBean.getAll();
 		return ResponseEntity.ok(orgs);
 	}
 
-	@PostMapping("/")
+	@PostMapping
 	public ResponseEntity<Organisation> postOne(@RequestBody Organisation organisation) {
 		Organisation org = organisationsBean.insertOne(organisation);
 		return new ResponseEntity<>(org, HttpStatus.CREATED);
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Organisation> getMethodName(@PathVariable Long id) {
+	public ResponseEntity<Organisation> getOne(@PathVariable Long id) {
 		Organisation org = organisationsBean.getById(id);
 		if (org != null) {
 			return new ResponseEntity<>(org, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Organisation> putOne(@PathVariable Long id, @RequestBody Organisation entity) {
+		boolean succ = organisationsBean.updateOne(id, entity);
+		if (succ) {
+			entity.setId(id);
+			return new ResponseEntity<>(entity, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Organisation> deleteOne(@PathVariable Long id) {
+		boolean succ = organisationsBean.deleteOne(id);
+		if (succ) {
+			return new ResponseEntity<>(HttpStatus.OK);
 		}
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
--- organisations/src/main/java/si/justphotons/organisations/services/beans/OrganisationsBean.java
+++ organisations/src/main/java/si/justphotons/organisations/services/beans/OrganisationsBean.java
@@ -1,10 +1,12 @@
 package si.justphotons.organisations.services.beans;
 
 import java.util.List;
+import java.util.Optional;
 
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import si.justphotons.organisations.entities.Organisation;
 import si.justphotons.organisations.entities.OrganisationsRepository;
 
@@ -28,6 +30,31 @@ public class OrganisationsBean {
 
   public Organisation getById(Long id) {
     return organisationsRepository.findById(id).orElse(null);
+  }
+
+  @Transactional
+  public boolean updateOne(Long id, Organisation org) {
+    Optional<Organisation> orgOptional = organisationsRepository.findById(id);
+    if (orgOptional.isPresent()) {
+      Organisation o = orgOptional.get();
+      o.setId(id);
+      o.setDescription(org.getDescription());
+      o.setName(org.getName());
+      organisationsRepository.save(o);
+      return true;
+    }
+    return false;
+  }
+
+  @Transactional
+  public boolean deleteOne(Long id) {
+    Optional<Organisation> org = organisationsRepository.findById(id);
+    if (org.isPresent()) {
+      organisationsRepository.deleteById(id);
+      return true;
+    }
+    
+    return false;
   }
 
 }
