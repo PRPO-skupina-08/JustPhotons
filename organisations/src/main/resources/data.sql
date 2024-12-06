abca32c9e78812c202fc0526811f5b2fdafd5a93
ba-na-na-na-na <vojko.hysz@gmail.com>
2024-12-06 20:58:37 +0100

    initial SQL data insert

---
+++ organisations/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO organisations (id, name, description) VALUES (1, 'Skavti LJ2', 'Najbolj kul steg ever. Kao.');
+INSERT INTO organisations (id, name, description) VALUES (2, 'Skavti LJ3', 'Srednja žalost.');
+INSERT INTO organisations (id, name, description) VALUES (3, 'Mladi SDS', 'Matic je naš šef. Kao');
+INSERT INTO organisations (id, name, description) VALUES (4, 'Oracle', 'Mi smo pa mal bolj resni.');
