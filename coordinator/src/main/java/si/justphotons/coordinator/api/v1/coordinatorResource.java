79bc6f0feebe24d69ee690ea0cf6a6f5c5c93ae2
ba-na-na-na-na <vojko.hysz@gmail.com>
2024-12-20 12:50:39 +0100

    file structure INIT

---
+++ coordinator/src/main/java/si/justphotons/coordinator/api/v1/coordinatorResource.java
@@ -0,0 +1,10 @@
+package si.justphotons.coordinator.api.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class coordinatorResource {
+
+}
---
+++ coordinator/src/main/java/si/justphotons/coordinator/services/beans/CoordinatorBean.java
@@ -0,0 +1,8 @@
+package si.justphotons.coordinator.services.beans;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CoordinatorBean {
+
+}
