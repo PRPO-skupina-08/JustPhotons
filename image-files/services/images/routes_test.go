554aca9d1dda99cf425e5ce979905cbc4671508a
mteXD <matic.pozenel@gmail.com>
2024-12-24 13:18:22 +0100

    Changed tests to be more concise

--- image-files/services/images/routes_test.go
+++ image-files/services/images/routes_test.go
@@ -3,6 +3,7 @@ package images
 import (
 	"bytes"
 	"encoding/json"
+	"image-service/config"
 	"image-service/types"
 	"net/http"
 	"net/http/httptest"
@@ -34,107 +35,61 @@ func (m *mockImageStore) DeleteImage(id uint) (result *gorm.DB) {
 	return
 }
 
+func (h *Handler) abstracted_POST(payload types.InsertImagePayload, expected int) func(t *testing.T) {
+	return func(t *testing.T) {
+		marshalled, _ := json.Marshal(payload)
+		endpoint := config.APIVersion + "/images"
+
+		req, err := http.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := chi.NewRouter()
+
+		router.HandleFunc(endpoint, h.handlePostImage)
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != expected {
+			t.Errorf("expected status code %d, got %d", expected, rr.Code)
+		}
+	}
+}
+
 func TestImageServiceHandlers(t *testing.T) {
 	imageStore := &mockImageStore{}
 	handler := NewHandler(imageStore)
 
-	t.Run("Should fail due to empty data", func(t *testing.T) {
-		payload := types.InsertImagePayload{
+	t.Run("Should fail due to empty data", handler.abstracted_POST(
+		types.InsertImagePayload{
 			Filename: "name.jpg",
 			Data:     []byte{},
-		}
+		},
+		http.StatusBadRequest,
+	))
 
-		marshalled, _ := json.Marshal(payload)
-
-		req, err := http.NewRequest(http.MethodPost, "/api/v1/images", bytes.NewBuffer(marshalled))
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		rr := httptest.NewRecorder()
-		router := chi.NewRouter()
-
-		router.HandleFunc("/api/v1/images", handler.handlePostImage)
-		router.ServeHTTP(rr, req)
-
-		expected := http.StatusBadRequest
-		if rr.Code != expected {
-			t.Errorf("expected status code %d, got %d", expected, rr.Code)
-		}
-	})
-
-	t.Run("Should fail due to empty filename", func(t *testing.T) {
-		payload := types.InsertImagePayload{
+	t.Run("Should fail due to empty filename", handler.abstracted_POST(
+		types.InsertImagePayload{
 			Filename: "",
 			Data:     []byte{0xCA, 0xFE, 0xBA, 0xBE}, // ultimate Java sarcasm
-		}
+		},
+		http.StatusBadRequest,
+	))
 
-		marshalled, _ := json.Marshal(payload)
-
-		req, err := http.NewRequest(http.MethodPost, "/api/v1/images", bytes.NewBuffer(marshalled))
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		rr := httptest.NewRecorder()
-		router := chi.NewRouter()
-
-		router.HandleFunc("/api/v1/images", handler.handlePostImage)
-		router.ServeHTTP(rr, req)
-
-		expected := http.StatusBadRequest
-		if rr.Code != expected {
-			t.Errorf("expected status code %d, got %d", expected, rr.Code)
-		}
-	})
-
-	t.Run("Should fail due to not jpg", func(t *testing.T) {
-		payload := types.InsertImagePayload{
+	t.Run("Should fail due to not jpg", handler.abstracted_POST(
+		types.InsertImagePayload{
 			Filename: "name.random",
 			Data:     []byte{0xCA, 0xFE, 0xBA, 0xBE}, // ultimate Java sarcasm
-		}
+		},
+		http.StatusBadRequest,
+	))
 
-		marshalled, _ := json.Marshal(payload)
-
-		req, err := http.NewRequest(http.MethodPost, "/api/v1/images", bytes.NewBuffer(marshalled))
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		rr := httptest.NewRecorder()
-		router := chi.NewRouter()
-
-		router.HandleFunc("/api/v1/images", handler.handlePostImage)
-		router.ServeHTTP(rr, req)
-
-		expected := http.StatusBadRequest
-		if rr.Code != expected {
-			t.Errorf("expected status code %d, got %d", expected, rr.Code)
-		}
-	})
-
-	t.Run("Should correctly add new data entry", func(t *testing.T) {
-		payload := types.InsertImagePayload{
+	t.Run("Should correctly add new data entry", handler.abstracted_POST(
+		types.InsertImagePayload{
 			Filename: "name.jpg",
 			Data:     []byte{0xCA, 0xFE, 0xBA, 0xBE}, // ultimate Java sarcasm
-		}
-
-		marshalled, _ := json.Marshal(payload)
-
-		req, err := http.NewRequest(http.MethodPost, "/api/v1/images", bytes.NewBuffer(marshalled))
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		rr := httptest.NewRecorder()
-		router := chi.NewRouter()
-
-		router.HandleFunc("/api/v1/images", handler.handlePostImage)
-		router.ServeHTTP(rr, req)
-
-		expected := http.StatusCreated
-		if rr.Code != expected {
-			t.Errorf("expected status code %d, got %d", expected, rr.Code)
-		}
-	})
+		},
+		http.StatusCreated,
+	))
 }
