e6834d32ee400778ab4b0b1b453cb4752cab5e0f
mteXD <matic.pozenel@gmail.com>
2024-12-27 20:03:27 +0100

    Geting metadata by its ID

--- image-metadata/services/metadata/routes.go
+++ image-metadata/services/metadata/routes.go
@@ -1,9 +1,12 @@
 package metadata
 
 import (
+	"fmt"
 	"image-metadata/config"
 	"image-metadata/types"
+	"image-metadata/utils"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -28,11 +31,31 @@ func (h *Handler) CreateRoutes(parentRouter *chi.Mux) {
 	subrouter.Use(middleware.Logger)
 
 	subrouter.Get("/{id}", h.handleGetMetadata)
+	subrouter.Get("/", h.handleGetAllMetadata)
 	subrouter.Post("/", h.handlePostMetadata)
 	subrouter.Delete("/{id}", h.handleDeleteMetadata)
 }
 
 func (h *Handler) handleGetMetadata(w http.ResponseWriter, r *http.Request) {
+	paramId := chi.URLParam(r, "id")
+	id, err := strconv.ParseUint(paramId, 10, 64)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+
+	// retrieve image / check if image exists
+	img, result := h.store.GetMetadataById(uint(id))
+	if result == nil {
+		utils.WriteError(w, http.StatusInternalServerError, fmt.Errorf("Internal server error: DB query result is nil"))
+	} else if result.Error != nil {
+		utils.WriteError(w, http.StatusNotFound, fmt.Errorf("Image with ID %v doesn't exist. Error: %v", id, result.Error))
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, img)
+}
+
+func (h *Handler) handleGetAllMetadata(w http.ResponseWriter, r *http.Request) {
 	// TODO
 }
 
