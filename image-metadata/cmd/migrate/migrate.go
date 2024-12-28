1c90eaedc3b3d0076eb1d2561f0c6851cd4b72ad
mteXD <matic.pozenel@gmail.com>
2024-12-28 17:36:37 +0100

    Enhanced migrate.go
    migrate.go now allows for dropping the database and will give more
    extensive description on how to use the program when used incorrectly.

--- image-metadata/cmd/migrate/migrate.go
+++ image-metadata/cmd/migrate/migrate.go
@@ -1,16 +1,57 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"image-metadata/cmd/initializers"
 	"image-metadata/types"
+	"os"
+
+	"gorm.io/gorm"
 )
 
-func init() {
-}
+type action func(db *gorm.DB)
 
-func main() {
+func run_action(actionName *string) {
+	if actionName == nil {
+		fmt.Fprintf(os.Stderr, "Error: something went horribly wrong if this pointer is nil!\n")
+		return
+	}
+
+	// Initialize the database
 	db, sqlDB := initializers.InitStorage()
 	defer sqlDB.Close()
 
+	// Define a map that associates strings with action functions
+	actions := map[string]action{
+		"":        migrate,
+		"migrate": migrate,
+		"drop":    drop,
+	}
+
+	// Get the action function based on the actionName
+	action, exists := actions[*actionName]
+	if !exists {
+		fmt.Fprintf(os.Stderr, "Error: action '%s' does not exist\n", *actionName)
+		flag.PrintDefaults()
+		return
+	}
+
+	// Run the action
+	action(db)
+}
+
+func migrate(db *gorm.DB) {
 	db.AutoMigrate(&types.Metadata{})
+}
+
+func drop(db *gorm.DB) {
+	db.Migrator().DropTable(&types.Metadata{})
+}
+
+func main() {
+	action := flag.String("action", "", "Action to perform. Allowed actions: `migrate` (runs when flag isn't present), `drop`")
+	flag.Parse()
+
+	run_action(action)
 }
